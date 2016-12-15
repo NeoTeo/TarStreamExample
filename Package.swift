73d45b62d326b20438c56406be586c323f21cc33
Teo Sartori <m@sartori.dk>
2016-12-15 09:03:27 +0100

    Change to use HttpStream as a module.

--- Package.swift
+++ Package.swift
@@ -5,5 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/NeoTeo/CallbackStreams.git", majorVersion: 0),
         .Package(url: "https://github.com/NeoTeo/TarStream.git", majorVersion: 0),
+        .Package(url: "https://github.com/NeoTeo/HttpStream.git", majorVersion: 0),
     ]
 )
