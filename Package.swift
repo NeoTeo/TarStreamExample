d53023c53779c35aeff92caa072eb1dc26b27e2c
Teo Sartori <m@sartori.dk>
2017-03-15 17:20:36 +0100

    Remove redundant package inclusion.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "TarStreamExample",
     dependencies: [
-        .Package(url: "https://github.com/NeoTeo/CallbackStreams.git", majorVersion: 0),
         .Package(url: "https://github.com/NeoTeo/TarStream.git", majorVersion: 0),
         .Package(url: "https://github.com/NeoTeo/HttpStream.git", majorVersion: 0),
     ]
