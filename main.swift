f2567f28c165b05b0afe415115a6fd8d268c242d
Teo Sartori <m@sartori.dk>
2017-03-16 12:42:52 +0100

    Add another example.

--- main.swift
+++ main.swift
@@ -30,7 +30,8 @@ func main() {
     
 //    tarStreamWriter()
 //    tarStreamWriter2()
-  tarStreamWriter3()
+//    tarStreamWriter3()
+  tarStreamWriter4()
 //      simplePipe()
     
     CFRunLoopRun()
@@ -273,6 +274,43 @@ func tarStreamWriter3() {
     tarStr.pipe(into: writeStream) { exit(EXIT_SUCCESS) }
 }
 
+
+func tarStreamWriter4() {
+
+    /// Set up a read stream and feed it a string as input.
+    guard let d = "A simple stream of characters.".data(using: .utf8) else { fatalError("Invalid string!") }
+    let readStream = InputStream(data: d)
+
+    /// Create a tar stream instance and get a new archive from it.
+    let tar = TarStream()
+    let archive = tar.archive()
+
+    // Add an entry to the archive and finalize it.
+    archive.addEntry(header: [TarHeader.Field.fileName : "file.txt"], dataStream: readStream)
+
+    /// File byte sizes in octal.
+    var entry: TarEntry = archive.addEntry(header: [.fileName : "greeting.txt", .fileByteSize : "12"]) {
+        /// This is the entry end handler. 
+        archive.closeArchive()
+    }
+    
+    entry.write(data: "Hej")
+    entry.write(data: " ")
+    entry.write(data: "Verden")
+    entry.end()
+
+    /// Output content of archive to stdout:
+
+    /// Get the read stream from the archive. 
+    guard let tarStr = archive.tarReadStream else { fatalError("Cannot read archive!") }
+
+    /// Create write stream to stdout and pipe the archive to it.
+    guard let writeStream = OutputStream(toFileAtPath: "/dev/stdout", append: false) else {
+    fatalError("Cannot create output stream!")
+}
+
+tarStr.pipe(into: writeStream) { exit(EXIT_SUCCESS) }
+}
 
 
 /**
